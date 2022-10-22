e2799a9432190bf7467477904fc33fe46b29bd2b
Darina Melis <darina05@email.cz>
2022-10-22 21:07:55 +0200

    Initial Commit

---
+++ MyTravelApp/MyTravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTravelAppApp.swift
+//  MyTravelApp
+//
+//  Created by Daryna Polevyk on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyTravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
